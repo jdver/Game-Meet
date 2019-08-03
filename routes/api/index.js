1397995e5690d9b7aeca87063c64f7bcbd2f51da
Andrew Kjar <andrewkjar34@gmail.com>
2019-08-03 13:37:54 -0700

    got user collection connected and to show in console

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 const gameRoutes = require('./games')
-// const userRoutes = require('./users')
+const userRoutes = require('./users')
 
 // Game routes
 router.use('/games', gameRoutes)
 
 // User routes
-// router.use('/users', userRoutes)
+router.use('/users', userRoutes)
 
 module.exports = router
