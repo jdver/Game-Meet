f9548877c0675c7cf453307cc187a19ffb0c7c99
Andrew Kjar <andrewkjar34@gmail.com>
2019-08-04 22:21:31 -0700

    user auth function and other general functions

--- client/src/components/modals/modalLogin.js
+++ client/src/components/modals/modalLogin.js
@@ -1,42 +1,80 @@
 import React from 'react'
-import { Button, Modal } from 'react-bootstrap'
+import { Route } from 'react-router-dom'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
+import API from '../../utils/API.js'
 
-function ModalLogin () {
-  const [show, setShow] = React.useState(false)
-  const handleClose = () => setShow(false)
-  const handleShow = () => setShow(true)
 
-  return (
+class ModalLogin extends React.Component {
+  state = {
+    modalIsOpen: false,
+    users: [],
+    username: '',
+    password: ''
+  }
+
+  toggleModal = () => {
+    this.setState({ modalIsOpen: ! this.state.modalIsOpen })
+  }
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = () => {
+    API.getUsers()
+      .then(response => response.json())
+      .then(res => {
+        this.setState({ users: res })
+        this.state.users.forEach((data) => {
+          if (this.state.username === data.username && this.state.password === data.password) {
+            alert('Your Are logged in')
+          } else {
+            alert('Please make sure your username and password are correct')
+          }  
+        })
+      })
+      .catch(err => console.log(err));
+  };
+
+  render () {
+    return (
       <>
-        <Button variant='primary' className='btn btn-dark nav-item text-white' onClick={handleShow}>
+      <Button variant='primary' className='btn btn-dark nav-item text-white' onClick={this.toggleModal.bind(this)}>
         Login
-        </Button>
-
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Login</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <label htmlFor='username'>Username</label>
-            <div className='form-group'>
-              <input type='text' className='form-control' id='username' placeholder='Enter username!' />
-            </div>
-            <label htmlFor='password'>Password</label>
-            <div className='form-group'>
-              <input type='password' className='form-control' id='password' placeholder='Enter password!' />
-            </div>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant='secondary' onClick={handleClose}>
-              Submit
-            </Button>
-            <Button variant='secondary' onClick={handleClose}>
-              Close
-            </Button>
-          </Modal.Footer>
-        </Modal>
+      </Button>
+  
+      <Modal isOpen={this.state.modalIsOpen}>
+        <ModalHeader toggle={this.toggleModal.bind(this)}>Login</ModalHeader>
+        <ModalBody>
+          <label htmlFor='username'>Username</label>
+          <div className='form-group'>
+            <input type='text' className='form-control' id='username' placeholder='Enter username!' value={this.state.username} onChange={this.handleInputChange} name="username" />
+          </div>
+          <label htmlFor='password'>Password</label>
+          <div className='form-group'>
+            <input type='password' className='form-control' id='password' placeholder='Enter password!' value={this.state.password} onChange={this.handleInputChange} name="password" />
+          </div>
+        </ModalBody>
+        <ModalFooter> 
+          <Button variant='secondary' onClick={this.handleFormSubmit}>
+            Submit
+          </Button>
+          <Route render={({ history }) => (
+              <Button
+                variant='secondary'
+                onClick={() => { history.push('/games') }}
+              >
+                Close
+              </Button>
+          )} />
+        </ModalFooter>
+      </Modal>
       </>
-  )
+    )
+  }
 }
 
 export default ModalLogin
